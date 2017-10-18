1a9fa6279b46e22567e6efe9db8447ea85fb8d16
Xavier Dewever <xavier.dewever@telenet.be>
2017-10-18 20:28:24 +0200

    Add project files.

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ProductsApp.Models; // meegeven dat we met deze specifieke models willen werken
+
+namespace ProductsApp.Controllers
+{
+    [Produces("application/json")]
+    
+    public class ProductsController : Controller // erft over van controller klasse
+    {
+        private Product[] products = new Product[]
+        {
+            new Product {Id = 1, Name="Bananen", Category="Fruit", Price=1},
+            new Product {Id = 2, Name="Teddybeer", Category="Speelgoed", Price=3.75M},
+            new Product {Id = 3, Name="Harddisk", Category="Hardware", Price=16.99M}
+        };
+
+        [Route("api/Products")] // URL route 
+        public IEnumerable<Product> GetAllProducts() // list van producten // web API controller
+        {
+            return products;
+        }
+
+        [Route("api/Products/{id}")] // URL route 
+        public Product GetProduct(int id) // web API controller
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id); // we zoeken binnen products, naar het eerste product met een overeenkomend id
+            return product;
+        }
+
+    }
+
+
+}
---
+++ ProductsApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApp.Models
+{
+    public class Product
+    {
+
+        // zaken die we meesturen of ophalen via onze API (model)
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
